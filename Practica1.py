e3392e93e569c02254558a0794fe7721ef95f8a2
Francisco <francisco29032002@gmail.com>
2026-01-16 16:33:10 -0600

    sumas

--- Practica1.py
+++ Practica1.py
@@ -1,9 +1,34 @@
+def sumar(): #Todo mal GG
+    while True:
+        print("\n[____Suma____] \nIngresa numeros positivos o negativos \nEscribe '=' para obtener el resultado\n")
+        total=0
+        while True:
+            caracter = input("numero o '=': ") 
+            if caracter == "=":
+                print(f"\nEl resultado es: {total}")
+                break
+            else:
+                try:
+                    numero = float(caracter)
+                    total += numero
+                except ValueError:
+                    print("\nError, Ingrese un numero o '='")
+        while True:
+            salir = input("\n Deseas hacer otra suma? (s/n):").lower()
+            if salir == "s":
+                break
+            elif salir == "n":
+                print("\nRegresando al menu\n")
+                return
+            else:
+                print("\nColoca una opcion valida  (s/n)")
+                        
 menu = 0
 while (menu != "9"):
     menu = input("Menu  \n1.-Suma \n2.-Multiplicacion \n3.-Division \n4.-Factorial \n5.-Tablas de multiplicar \n6.-Cudrado y cubo \n7.-Promedio \n8.-Mayor y minimo \n9.-Salir\nIngrese una opcion: ")
     match menu:
         case "1":
-            print("\nSuma\n")
+            sumar()
         case "2":
             print("\nMultiplicacion\n")
         case "3":
@@ -22,3 +47,4 @@ while (menu != "9"):
             print("\nSaliendo....\n")
         case _:
             print("\nSeleccione una opcion valida\n")
+
